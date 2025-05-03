a4c8912e5d0d640915841c25a9be75329c156016
cptfran <frankiszymon@gmail.com>
2025-05-03 19:24:57 +0200

    Initial commit

---
+++ src/main/java/com/github/cptfran/kafka_batch_ingestor/KafkaBatchIngestorApplication.java
@@ -0,0 +1,13 @@
+package com.github.cptfran.kafka_batch_ingestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaBatchIngestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaBatchIngestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/cptfran/kafka_batch_ingestor/KafkaBatchIngestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cptfran.kafka_batch_ingestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaBatchIngestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
